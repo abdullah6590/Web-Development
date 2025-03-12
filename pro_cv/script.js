3cf9482832fa3a03f1d8e73630962929a74335db
abdullah6590 <abdullahshah2003@gmail.com>
2025-03-12 07:44:16 +0500

    pro cv

---
+++ pro_cv/script.js
@@ -0,0 +1,8 @@
+// Example JavaScript to add interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.btn');
+
+    btn.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
